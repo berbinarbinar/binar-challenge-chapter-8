d525ec77ce7d719cc772c047e12760cfbabe84e2
Jeremiah Prasetyo <93666214+jepra777@users.noreply.github.com>
2022-03-15 14:17:07 +0700

    Jere-API-Documentation

--- server/config/config.js
+++ server/config/config.js
@@ -1,24 +1,27 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME_DEV || "postgres",
-    "password": process.env.DB_PASSWORD_DEV || "postgres",
-    "database": process.env.DB_DEV || "c8-dev",
+    "username": process.env.DB_USERNAME_DEV,
+    "password": process.env.DB_PASSWORD_DEV,
+    "database": process.env.DB_DEV,
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "port": 35777
   },
   "test": {
     "username": process.env.DB_USERNAME_TEST,
     "password": process.env.DB_PASSWORD_TEST,
     "database": process.env.DB_TEST,
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "port": 35777
   },
   "production": {
     "username": process.env.DB_USERNAME_PROD,
     "password": process.env.DB_PASSWORD_PROD,
     "database": process.env.DB_PROD,
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "port": 35777
   }
 }
