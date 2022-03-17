fa7966a8f2b31bffe10d9795692538366a542115
icanq <icanq@hotmail.com>
2022-03-17 20:16:06 +0700

    swag abies

--- app.js
+++ app.js
@@ -5,15 +5,55 @@ const apiRouter = require('./server/routes')
 const errorHandler = require('./server/middlewares/errorHandler')
 const PORT = process.env.PORT || 4000
 
+// swagger
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+
 // middlewares
 app.use(cors())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(errorHandler)
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Binar Academy Chapter 8 - Player Documentations",
+      description: "API documentation for fulfill the challenge task in binar",
+      contact: {
+        name: "icanq"
+      },
+      servers: ["http://localhost:4000"]
+    }
+  },
+  apis: ["app.js", "./server/**/*.js"]
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 /**
  * @Routes /api
  * entrypoint for all API routes
+ */
+
+/**
+ * @rules
+ * rules penulisan swagger
+ * 1. indentasi haruslah baik
+ * 2. dalam satu rute, namun ada request method berbeda bisa menggunakan dalam comment@swagger. yang sama
+ * 3. apabila rute berbeda misal, harus pakai parameter /api/v1/players/:id, harus menggunakan comment@swagger. yang baru
+ */
+/**
+ * @swagger
+ * /api/v1:
+ *  get:
+ *    description: Use to be the entrypoint for /api/v1
+ *    responses:
+ *      200:
+ *        description: test
+ *  post:
+ *    description: use to be post
  */
 app.use("/api", apiRouter)
 
--- server/routes/v1/player.routes.js
+++ server/routes/v1/player.routes.js
@@ -5,8 +5,78 @@ const playerRouter = require("express").Router();
  * @Routes "/api/v1/players"
  */
 
+// extended version please read this
+// https://swagger.io/docs/
+// https://swagger.io/docs/specification/describing-responses/
+/**
+ * @swagger
+ * /api/v1/players:
+ *  get:
+ *    description: "To get players data from database"
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      '200':
+ *        description: Array of players
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ * 
+ *  post:
+ *    description: "to post players data"
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: email
+ *        in: body
+ *        required: true
+ *        example: icanq@hotmail.com
+ *      - name: username
+ *        in: body
+ *        required: true
+ *        example: DoomMagnet613
+ *      - name: password
+ *        in: body
+ *        required: true
+ *        example: Wokeoke123.
+ */
+
 playerRouter.get("/", PlayerController.getPlayers);
 playerRouter.post("/", PlayerController.createPlayer);
+
+/**
+ * @swagger
+ *  /api/v1/players/{id}:
+ *    get:
+ *      summary: Retrieve a single player user
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          example: 1
+ *          schema:
+ *            type: string
+ *          required: true
+ *      description: Retrieve a single player user. Can be used to populate
+ *      responses:
+ *        200:
+ *          description: A single user
+ *          content:
+ *            application/json:
+ *              properties:
+ *                schema:
+ *                  type: object
+ *                  properties:
+ *                      id:
+ *                        type: integer
+ *                        description: The Player ID
+ *                        example: 1
+ *                      username:
+ *                        type: string
+ *                        description: The Player Username 
+ *                        example: DoomSlayer212
+ */
+
 playerRouter.get("/:id", PlayerController.getPlayerById);
 playerRouter.put("/:id", PlayerController.updatePlayer);
 playerRouter.delete("/:id", PlayerController.deletePlayer);
