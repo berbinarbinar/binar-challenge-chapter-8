fe587fc8d0955b6899f69ddfcd3e1851ee8f88c5
kelvin handoko <kelvinhandoko03@gmail.com>
2022-03-28 02:53:47 +0700

    swag n client initialize

--- app.js
+++ app.js
@@ -5,15 +5,52 @@ const apiRouter = require('./server/routes')
 const errorHandler = require('./server/middlewares/errorHandler')
 const PORT = process.env.PORT || 4000
 
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+
 // middlewares
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(errorHandler)
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.3',
+    info: {
+      title: 'Binar Academy Chapter 8',
+      version: '1.0.0',
+      description: 'API doc for Binar challenge Chapter 8',
+      contact: {
+        name: 'Kelvin Handoko'
+      },
+      servers: {
+        url: `http://localhost:${process.env.PORT}`,
+        description: 'development server'
+      }
+    }
+  },
+  apis: ['app.js', './server/**/*.js']
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 /**
  * @Routes /api
  * entrypoint for all API routes
+ */
+
+/**
+ * @swagger
+ * /api/v1:
+ *  get:
+ *    description: Use to be the entrypoint for /api/v1
+ *    responses:
+ *      200:
+ *        description: test
+ *  post:
+ *    description: use to be post
  */
 app.use("/api", apiRouter)
 
--- server/routes/v1/player.routes.js
+++ server/routes/v1/player.routes.js
@@ -12,4 +12,253 @@ playerRouter.put("/:id", PlayerController.updatePlayer);
 playerRouter.delete("/:id", PlayerController.deletePlayer);
 playerRouter.post("/exp/:id", PlayerController.updateExperience);
 
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Player:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: id of player
+ *        username:
+ *          type: string
+ *          description: username of player
+ *        email:
+ *          type: string
+ *          description: email of player
+ *        password:
+ *          type: string
+ *          description: password of player
+ *        experience:
+ *          type: integer
+ *          description: experience of player
+ *        lvl:
+ *          type: integer
+ *          description: level of player
+ *        createdAt:
+ *          type: string
+ *          description: player creation date
+ *        updatedAt:
+ *          type: string
+ *          description: last modified date
+ * 
+ *  response:
+ *    DataPlayer:
+ *      description: OK
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties: 
+ *              result:
+ *                type: string
+ *                example: Success
+ *                description: result of fetch
+ *              data:
+ *                type: array
+ *                description: array of object player data
+ *                items:
+ *                  $ref: '#/components/schemas/Player'
+ *    Success:
+ *      description: OK
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties: 
+ *              result:
+ *                type: string
+ *                description: result of fetch
+ *                example: Success
+ *              message:
+ *                type: string
+ *                description: message of failed
+ *    Failed:
+ *      description: Bad Request
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties: 
+ *              result:
+ *                type: string
+ *                description: result of fetch
+ *                example: Failed
+ *              message:
+ *                type: string
+ *                description: message of failed
+ * 
+ *  request:
+ *    Account:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                description: username of player
+ *              email:
+ *                type: string
+ *                description: email of player
+ *              password:
+ *                type: string
+ *                description: password of player
+ *            required:
+ *              - username
+ *              - email
+ *              - password
+ *          examples:
+ *            player:
+ *              summary: example value body
+ *              value: 
+ *                username: anakBaik2
+ *                email: budiman1@gmail.com
+ *                password: test1
+ *    Exp:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              exp:
+ *                type: integer
+ *                description: exp earned by player
+ *            required:
+ *              - exp
+ *          examples:
+ *            exp:
+ *              summary: example add exp player
+ *              value:
+ *                exp: 10000
+ * 
+ * 
+ * paths:
+ *  /api/v1/players:
+ *    get:
+ *      summary: Finds players
+ *      description: >
+ *        Find all player if query is empty Or
+ *        Filter player by query if query is exist
+ *      tags:
+ *        - players
+ *      produces:
+ *        - application/json
+ *      parameters:
+ *        - in: query
+ *          name: username
+ *          schema:
+ *            type: string
+ *            example: stillDreaming21
+ *          description: the username of player
+ *        - in: query
+ *          name: email
+ *          schema:
+ *            type: string
+ *          description: the email of players
+ *        - in: query
+ *          name: experience
+ *          schema:
+ *            type: integer
+ *          description: the experience of players
+ *        - in: query
+ *          name: lvl
+ *          schema:
+ *            type: integer
+ *          description: the level of players
+ *      responses:
+ *        '200':
+ *          $ref: '#/components/response/DataPlayer'
+ *    post:
+ *      summary: Create player
+ *      tags:
+ *        - players
+ *      procudes:
+ *        - application/json
+ *      requestBody:
+ *        $ref: '#/components/request/Account'
+ *      responses:
+ *        '201':
+ *          $ref: '#/components/response/DataPlayer'
+ *        '400':
+ *          $ref: '#/components/response/Failed'
+ * 
+ *  /api/v1/players/{id}:
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        description: the player id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *          example: 1                   
+ *    get:
+ *      summary: get a player by id
+ *      tags:
+ *        - players
+ *      procudes:
+ *        - application/json
+ *      responses:      
+ *        '200':                
+ *          $ref: '#/components/response/DataPlayer' 
+ *        '404':
+ *          $ref: '#/components/response/Failed'   
+ *    put:
+ *      summary: update account player  
+ *      tags:       
+ *        - players            
+ *      procudes:
+ *        - application/json
+ *      requestBody:
+ *        $ref: '#/components/request/Account'
+ *      responses:
+ *        '200':
+ *          $ref: '#/components/response/Success'
+ *        '400':
+ *          $ref: '#/components/response/Failed'
+ *        '404':
+ *          description: Player Not Found
+ *          $ref: '#/components/response/Failed'
+ *    delete:
+ *      summary: delete player
+ *      tags:
+ *        - players
+ *      procudes:
+ *        - application/json
+ *      responses:
+ *        '200':
+ *          $ref: '#/components/response/Success'
+ *        '400':
+ *          $ref: '#/components/response/Failed'
+ *  
+ *  /api/v1/players/exp/{id}:
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        description: the player id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *          example: 1
+ *    post:
+ *      summary: update experience player
+ *      tags: 
+ *        - players
+ *      procudes:
+ *        - application/json
+ *      requestBody:
+ *        $ref: '#/components/request/Exp'
+ *      responses:
+ *        '200':
+ *          $ref: '#/components/response/Success'
+ *        '400':
+ *          $ref: '#/components/response/Failed'
+ *        '404':
+ *          $ref: '#/components/response/Failed'    
+ */
 module.exports = playerRouter;
