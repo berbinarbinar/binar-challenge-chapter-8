b1eaacfbcfce04d95ca09f579dc09e7cad8e162e
Jeremiah Prasetyo <jere.prasetyo.jp@gmail.com>
2022-03-27 22:32:37 +0700

    API Documentation Postman & Swagger

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const swaggerOptions = {
   //untuk mendefinisikan API yang kita buat
   swaggerDefinition:{
     info: {
-      title: "Jere - API Documentation with Swager on Chapter 8",
-      description: "API documentation for fullfill the challenge task on Chapter 8",
+      title: "Chapter 8 - JePra",
+      description: "This is the API Documentation of Chapter 8 on Binar Academy to CRUD (Create, Read, Update and Delete) Players",
       contact: {
         name: "JePra"
       },
@@ -40,14 +40,153 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
  */
 
 /**
+ * @swagger
+ * /api/v1/players/:
+ *  post:
+ *    tags:
+ *      - Players API 
+ *    summary: This is the Create API to create a Player
+ *    consumes:
+ *      - application/json
+ *    parameters:
+ *      - in: body
+ *        name: Create Player
+ *        description: This is the Create API to create a Player
+ *        schema:
+ *          type: object
+ *          required:
+ *            - username
+ *            - email
+ *            - password
+ *          properties:
+ *            username:
+ *              type: string
+ *            email:
+ *              type: string
+ *            password:
+ *              type: string
+ *            experience:
+ *              type: integer
+ *    responses:
+ *      201:
+ *        description: The Player Has Been Created!
+ */
+
+/**
  * @swagger 
- * /api/v1/players:
+ * /api/v1/players/:
  *  get:
- *    description: Use
+ *    tags:
+ *       - Players API 
+ *    summary: This is the Read API to Get the lists of Players
+ *    description: This is the Read API to Get the lists of Players
  *    responses:
  *      200:
  *        description: Show All Players
  */
+
+/**
+ * @swagger
+ * /api/v1/players/{id}:
+ *   get:
+ *     tags:
+ *       - Players API 
+ *     summary: This is the Read API to Get the list of Player by ID
+ *     description: This is the Read API to Get the list of Player by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: This is the User ID
+ *     responses:
+ *       200:
+ *         description: Show The Player by ID
+ */
+
+/**
+ * @swagger
+ * /api/v1/players/{id}:
+ *   put:
+ *     tags:
+ *       - Players API 
+ *     summary: This is the Update API to Update the Player Data using Put
+ *     description: This is the Update API to Update the Player Data using Put
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: This is the User ID
+ *       - in: body
+ *         name: Update Player
+ *         description: This is the Update API to Update the Player Data using Put
+ *         schema:
+ *          type: object
+ *          required: 
+ *            - username
+ *            - email
+ *            - password
+ *          properties:
+ *            username:
+ *              type: string
+ *            email:
+ *              type: string
+ *            password: 
+ *              type: string
+ *     responses:
+ *       200:
+ *         description: Update The Player by ID
+ */
+
+/**
+ * @swagger
+ * /api/v1/players/exp/{id}:
+ *   post:
+ *     tags:
+ *       - Players API 
+ *     summary: This is the Update API to Update the Player Data using Post
+ *     description: This is the Update API to Update the Player Data using Post
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: This is the User ID
+ *       - in: body
+ *         name: Update Player
+ *         description: This is the Update API to Update the Player Data using Put
+ *         schema:
+ *          type: object
+ *          required: 
+ *            - exp
+ *          properties:
+ *            exp:
+ *              type: integer
+ *     responses:
+ *       200:
+ *         description: Update The Player Exp by ID
+ */
+
+/**
+ * @swagger
+ * /api/v1/players/{id}:
+ *   delete:
+ *     tags:
+ *       - Players API 
+ *     summary: This is the Delete API to Delete the Player by ID
+ *     description: This is the Delete API to Delete the Player by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: This is the User ID
+ *     responses:
+ *       200:
+ *         description: Delete The Player by ID
+ */
+
 app.use("/api", apiRouter)
 
 app.listen(PORT, () => {
