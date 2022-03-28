b0e233e778bac8301af0761ce22489a35a8f1e8b
Akbar Gunadi <gunadiakbar24@gmail.com>
2022-03-28 17:44:41 +0700

    Swagger and React Finished

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const apiRouter = require('./server/routes')
-const errorHandler = require('./server/middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const apiRouter = require('./server/routes');
+const errorHandler = require('./server/middlewares/errorHandler');
+const PORT = process.env.PORT || 4000;
+
+// swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 // middlewares
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(errorHandler)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
 
+const swaggerOptions = {
+  // untuk mendefenisikan API
+  swaggerDefinition: {
+    openapi: '3.0.3',
+    info: {
+      title: 'Binar Challenge Chapter 8',
+      version: '1.0.0',
+      description: 'API documentation for Binar Challenge Chapter 8',
+      contact: {
+        name: 'Akbar Gunadi',
+      },
+      servers: {
+        url: `http://localhost:${process.env.PORT}`,
+        description: 'development server',
+      },
+    },
+  },
+  apis: ['app.js', './server/**/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 /**
  * @Routes /api
  * entrypoint for all API routes
  */
-app.use("/api", apiRouter)
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
