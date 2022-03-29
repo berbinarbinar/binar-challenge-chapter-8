face5bd09fbf494e1111b0789cb620959684de94
kelvin handoko <kelvinhandoko03@gmail.com>
2022-03-30 03:33:35 +0700

    feat: delete unused button

--- client/src/components/searchPlayer.jsx
+++ client/src/components/searchPlayer.jsx
@@ -160,11 +160,6 @@ export default function Search() {
           <ModalBody>
             <EditForm player={modalData} />
           </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
