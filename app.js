ee3d97facbfcac75737e5cc35b73775f154de938
pashateuku <thariqteuku.soc@gmail.com>
2022-06-22 19:53:35 +0700

    add: swagger API documentation in /docs route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const apiRouter = require('./server/routes')
 const errorHandler = require('./server/middlewares/errorHandler')
 const PORT = process.env.PORT || 4000
 
+const swaggerJSON = require('./swagger.json');
+const swaggerUI = require('swagger-ui-express');
+
+
 // middlewares
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
@@ -16,6 +20,8 @@ app.use(errorHandler)
  * entrypoint for all API routes
  */
 app.use("/api", apiRouter)
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
+
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
